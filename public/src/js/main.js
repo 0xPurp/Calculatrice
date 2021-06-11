20c98643818c44f9639c4e19b549894b89cb3e46
0xPurp <haroune1020@hotmail.com>
2021-06-11 10:12:25 +0200

    push 04 : ajout HTML (Ajout de 5 lignes de boutons [chiffres + opérateur])

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -26,9 +26,28 @@ divs[1].append(input,clear)
 // ./div 2
 // Début div 3
 let bouton;
+// ligne 1
 for (let i = 0; i < 4; i++){
     bouton = document.createElement('button')
     divs[2].append(bouton);
-    const xBoutons = divs[2].getElementsByTagName('button');
+    const ligne1 = divs[2].getElementsByTagName('button');
+}
+// ligne 2
+for (let i = 0; i < 4; i++){
+    bouton = document.createElement('button')
+    divs[3].append(bouton);
+    const ligne2 = divs[3].getElementsByTagName('button');
+}
+// ligne 4
+for (let i = 0; i < 4; i++){
+    bouton = document.createElement('button')
+    divs[4].append(bouton);
+    const ligne3 = divs[4].getElementsByTagName('button');
+}
+// ligne 5
+for (let i = 0; i < 4; i++){
+    bouton = document.createElement('button')
+    divs[5].append(bouton);
+    const xBoutons = divs[5].getElementsByTagName('button');
 }
 // ./div 3
